841be12996cff55b327f94ef68ff10e0f93e0b6a
julianaarias12 <julianaarias123000@gmail.com>
2023-03-23 19:33:19 -0500

    creacion home con sus respectivos estilos

---
+++ src/pages/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.scss'
+
+const Header = () => {
+  return (
+    <div className='container-header'>
+        <div className='name-bussiness'>Contrataciones</div>
+        <div>Bienvenid@ Juliana</div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from '../components/header/Header'
+import { FaUserAlt, FaUserPlus , FaDoorClosed} from "react-icons/fa";
 import './index.scss'
 const Home = () => {
   return (
     <div className='container-home'>
-        <button>Aspirante Nuevo</button>
-        <button>Aspirante </button>
+      <Header />
+      <div className='container-buttons'>
+        <div className='top'>
+          <button>
+            <FaUserPlus />
+            <p>Crear empleado</p>
+          </button>
+          <button>
+            <FaUserAlt />
+            <p>Empleado</p>
+          </button>
+        </div>
+        <div className='bottom'>
+          <button>
+            <FaDoorClosed/>
+            <p>Cerrar sesion</p>
+            </button>
+        </div>
+      </div>
     </div>
   )
 }
