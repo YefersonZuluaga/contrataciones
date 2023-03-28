44e343989528efbacb536dc76c5bbd03cde56465
Yeferson Zuluaga <ahumada1248@gmail.com>
2023-03-27 23:23:15 -0500

    creacion auth firebase

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,82 +2,81 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { db } from '../../../firebase'
 import { collection, getDocs } from "firebase/firestore";
+import { message } from 'antd';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import './styles.scss'
 
 const Login = () => {
 
+    //estados y variables
+
+    const auth = getAuth();
     const navigate = useNavigate()
+    const [messageApi, contextHolder] = message.useMessage();
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const [user, setUser] = useState("")
+    const onSubmit = () => {
 
-    const onFinish = () => {
-        navigate("/home")
-        if(user != " "){
-
+        if ((email != "" && email != " ") && (password != "" && password != " ")) {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user)
+                    navigate("/home")
+                })
+                .catch((e) => {
+                    console.log("usuario no encontrado")
+                    error("Usuario incorrecto.")
+                    const errorCode = e.code;
+                    const errorMessage = e.message;
+                });
+        }else{
+            warning("Complete todos los campos.")
         }
     }
 
-    const obtenerDatos = async () => {
 
+    const obtenerDatos = async () => {
 
         const querySnapshot = await getDocs(collection(db, "usuarios"))
         querySnapshot.forEach((doc) => {
-            if(user == doc.id){
+            if (user == doc.id) {
+                navigate('/home')
                 console.log("entro")
             }
             // console.log(doc.id)
             // console.log(doc.data());
         });
-
-        // let coleccion = db.collection("usuarios")
-        // await coleccion.where("Codigo", "==", codigo)
-        // .onSnapshot(snapshot => {
-        //   snapshot.docs.map(doc => {
-        //     let result = doc.data()
-        //     setPreviewImage(result.urlImagen)
-        //     let fecha = result.FechaIngreso
-        //     let date = (fecha && fecha.toDate()) ? new Date(fecha.toDate()) : new Date();
-        //     let fechaFormat = moment(date).format("DD-MM-YYYY h:mm:ss")
-        //     setConexionUser(fechaFormat)
-            
-        //   })
-        // })
-
-        // const getImagenUser = async (codigo) => {
-
-
-        // let coleccion = db.collection("usuarios")
-        // await coleccion.where("nombre", "==", "1001113982")
-        // .onSnapshot(snapshot => {
-        //   snapshot.docs.map(doc => {
-        //     let result = doc.data()
-        //     console.log(result)
-        //     // setPreviewImage(result.urlImagen)
-        //     // let fecha = result.FechaIngreso
-        //     // let date = (fecha && fecha.toDate()) ? new Date(fecha.toDate()) : new Date();
-        //     // let fechaFormat = moment(date).format("DD-MM-YYYY h:mm:ss")
-        //     // setConexionUser(fechaFormat)
-
-        //   })
-        // })
-        //   }
     }
 
-    useEffect(() => {
-        obtenerDatos()
-    }, [])
+    const error = (message) => {
+        messageApi.open({
+            type: 'error',
+            content: message,
+        });
+    };
+
+    const warning = (message) => {
+        messageApi.open({
+          type: 'warning',
+          content: message,
+        });
+      };
 
 
     return (
         <div className='container-login'>
+            {contextHolder}
             <div className='container-logo'>
             </div>
             <div className='container-card-login'>
                 <div className='card'>
                     <h1>Inicio de sesión</h1>
-                    <input className='input' value={user} onChange={(e) => setUser(e.target.value)} placeholder='Identificacion' />
-                    <button onClick={onFinish}>Ingresar</button>
+                    <input className='input' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Correo' />
+                    <input className='input' value={password} type="password" onChange={(e) => setPassword(e.target.value)} placeholder='Contraseña' />
+                    <button onClick={onSubmit}>Ingresar</button>
                 </div>
             </div>
         </div>
