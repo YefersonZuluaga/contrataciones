0d634ca8105c641731c6bdd5e1f1660ab128d138
julianaarias12 <julianaarias123000@gmail.com>
2023-05-02 11:32:44 -0500

    ajuste pagina notfound

--- src/pages/notFound/notFound.jsx
+++ src/pages/notFound/notFound.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './styles.scss'
+import { ImSad } from "react-icons/im";
+import { useNavigate } from 'react-router-dom';
+
+
 
 const NotFound = () => {
+  const navigate = useNavigate()
   return (
     <div className='container-notFound'>
       <div>
+        <h1 className='icon'>
+      <ImSad />
+      </h1>
         <h1 className='titulo'>404</h1>
-        <p>NotFound</p>
+        <p className='contenido'>Page not found</p>
       </div>
+      <button onClick={()=> navigate("/home")} className='button'>Ir al incio</button>
+
     </div>
   )
 }
