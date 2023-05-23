5eafeb3238857175e39dbe1ad0ca4c5dacc7beb0
YefersonZuluaga <ahumada1248@gmail.com>
2023-05-23 01:41:39 -0500

    variable de entorno

--- firebase.js
+++ firebase.js
@@ -1,15 +1,19 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage, } from "firebase/storage";
+const { VITE_URL_APIKEY } = import.meta.env;
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCESrJWBn0YjtYKQAGxQ5spsw3YLSSXgAY",
+  apiKey: VITE_URL_APIKEY,
   // authDomain: "http://localhost:5173/",
   authDomain: "contrataci0nes.firebaseapp.com",
   projectId: "contrataci0nes",
